0f5df58af6b18592908358ffb0312b9158eec9c0
MedalSUN <2496667187@qq.com>
2019-04-27 21:35:03 +0800

    注册页面

---
+++ src/components/auth/Register.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, TouchableOpacity, Text } from 'react-native'
+// import { Button, Input, Image, Text } from 'react-native-elements'
+
+import Styles from './style'
+
+export default class Login extends React.Component {
+  render () {
+    return (
+      <View>
+        <Text>注册页</Text>
+      </View>
+    )
+  }
+}

2819bb8c5ff6eeca22c7f7d8b6e5d6bce876f0dd
MedalSUN <2496667187@qq.com>
2019-04-27 21:33:39 +0800

    首页新闻页面

---
+++ src/components/news/NewsMainPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, TouchableOpacity, Text } from 'react-native'
+// import { Button, Input, Image, Text } from 'react-native-elements'
+
+// import Styles from './style'
+
+export default class NewsMainPage extends React.Component {
+  render () {
+    return (
+      <View>
+        <Text>新闻首页</Text>
+      </View>
+    )
+  }
+}

1d4e7b5e44592974450860e24a9fd839cc3b7bb0
MedalSUN <2496667187@qq.com>
2019-04-27 21:33:47 +0800

    赛事页面

---
+++ src/components/race/RaceMainPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, TouchableOpacity, Text } from 'react-native'
+// import { Button, Input, Image, Text } from 'react-native-elements'
+
+// import Styles from './style'
+
+export default class RaceMainPage extends React.Component {
+  render () {
+    return (
+      <View>
+        <Text>赛事主页</Text>
+      </View>
+    )
+  }
+}

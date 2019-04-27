f251de5bd0e48d743a4a439d159c77a2ab6bd76b
MedalSUN <2496667187@qq.com>
2019-04-27 21:35:51 +0800

    登陆注册文件夹的样式

--- src/components/auth/style.js
+++ src/components/auth/style.js
@@ -1,4 +1,34 @@
 import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
+    loginStyle: {
+        backgroundColor: '#FDFEFE',
+        height: 670
+    },
+    logoStyle: {
+        width: 200,
+        height: 200,
+        marginLeft:110
+    },
+    loginInputContainer:{
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingLeft: 50,
+        marginTop: 80
+    },
+    loginInputStyle: {
+        width: 300
+    },
+    submitButtonStyle: {
+        marginTop: 50,
+        marginLeft: -30
+    },
+    registerTextContainerStyle: {
+        marginTop: 50,
+        marginLeft: -30
+    },
+    registerTextStyle: {
+        fontSize: 20
+    }
 })

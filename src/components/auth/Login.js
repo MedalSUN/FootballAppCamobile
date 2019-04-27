bd22967f433e1a93712568b6740ec5429232cecc
MedalSUN <2496667187@qq.com>
2019-04-27 21:35:09 +0800

    登陆页面

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,21 +1,43 @@
+/* eslint-disable no-lone-blocks */
 import React from 'react'
+import { View, TouchableOpacity, Text } from 'react-native'
+// import { Button, Input, Image, Text } from 'react-native-elements'
 
-// import {View} from 'react-native'
-
-import { View, Text } from 'native-base'
+import Styles from './style'
 
 export default class Login extends React.Component {
-  constructor (props) {
-    super(props)
-  }
-
-
   render () {
     return (
-
-      <View>
-            <Text>登陆页</Text>
+      <View style={Styles.loginStyle}>
+        <Text>登陆页</Text>
       </View>
     )
   }
 }
+
+{ /* <Image
+source={require( '../../../img/logo.png' )}
+style={Styles.logoStyle}
+/>
+<View style={Styles.loginInputContainer}>
+<Input
+  placeholder='请输入邮箱'
+  inputContainerStyle={Styles.loginInputStyle}
+/>
+<Input
+  placeholder='请输入密码'
+  inputContainerStyle={Styles.loginInputStyle}
+/>
+<View style={Styles.submitButtonStyle}>
+  <Button
+    title='登录'
+    type='solid'
+  />
+</View>
+<TouchableOpacity onPress={() => console.log('欢迎跳到注册页')}>
+  <View style={Styles.registerTextContainerStyle}>
+    <Text style={Styles.registerTextStyle}>注册</Text>
+  </View>
+</TouchableOpacity>
+</View>
+*/ }

8e4aae99e15a699b6acfd62258d8a57d3e983cda
MedalSUN <2496667187@qq.com>
2019-05-04 20:39:19 +0800

    测试显示图片demo的样式文件

---
+++ src/components/self/style.js
@@ -0,0 +1,9 @@
+/* eslint-disable react-native/no-color-literals */
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  imgSize: {
+    width: 20,
+    height: 20
+  }
+})

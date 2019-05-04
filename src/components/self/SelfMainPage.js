04382182a4503b015175aa18b94f94eb7e221fe3
MedalSUN <2496667187@qq.com>
2019-05-04 20:39:02 +0800

    使用百度网盘代替图片上传服务器，测试能否正常显示图片

--- src/components/self/SelfMainPage.js
+++ src/components/self/SelfMainPage.js
@@ -1,15 +1,64 @@
-import React from 'react'
-import { View, TouchableOpacity, Text } from 'react-native'
-// import { Button, Input, Image, Text } from 'react-native-elements'
+// import React from 'react'
+// import { View, TouchableOpacity, Text } from 'react-native'
+// // import { Button, Input, Image, Text } from 'react-native-elements'
 
-// import Styles from './style'
+// // import Styles from './style'
 
-export default class SelfMainPage extends React.Component {
-  render () {
-    return (
-      <View>
-        <Text>我的主页</Text>
-      </View>
-    )
+// export default class SelfMainPage extends React.Component {
+//   render () {
+//     return (
+//       <View>
+//         <Text>我的主页</Text>
+//       </View>
+//     )
+//   }
+// }
+
+import React, { Component } from 'react'
+import { Image, Text } from 'react-native'
+import { Container, Content, View } from 'native-base'
+import { Query } from 'react-apollo'
+import Styles from './style'
+import gql from 'graphql-tag'
+
+const ALL_IMG = gql`
+query allImages{
+  allImages{
+    nodes{
+      url
+    }
   }
 }
+`
+
+class GiftsScreen extends Component {
+  render () {
+    return < Query query={ALL_IMG} >
+      {({ loading, error, data }) => {
+        if (loading) return <Text>Loading...</Text>
+        if (error) return <Text>`Error! ${error.message}`</Text>
+        return (
+          <Container>
+            <Content>
+              <View>
+                {
+                  data.allImages.nodes.map((p, i) => {
+                    return (
+                      <View key={i} >
+                        <View>
+                          <Image source={{ uri: p.url }} style={Styles.imgSize}/>
+                        </View>
+                      </View>
+                    )
+                  })
+                }
+              </View>
+            </Content>
+          </Container>
+        )
+      }
+      }
+    </Query>
+  }
+}
+export default GiftsScreen

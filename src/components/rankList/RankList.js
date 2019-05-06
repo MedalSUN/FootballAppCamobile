c4d38d93fb4348a3da7cf184fe1549f1eb00f404
MedalSUN <2496667187@qq.com>
2019-05-06 22:18:41 +0800

    新增榜单模块页面

---
+++ src/components/rankList/RankList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Image, Text } from 'react-native'
+import { Container, Content, View } from 'native-base'
+
+class RankList extends Component {
+  render () {
+    return (
+      <Container>
+        <Content>
+          <View>
+            <Text>榜单</Text>
+          </View>
+        </Content>
+      </Container>
+    )
+  }
+}
+export default RankList

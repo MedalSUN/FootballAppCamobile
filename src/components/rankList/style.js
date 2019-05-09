3842ba1faad92acea25ad67ee89d0e2a1c3c8deb
MedalSUN <2496667187@qq.com>
2019-05-09 15:10:48 +0800

    改背景颜色

---
+++ src/components/rankList/style.js
@@ -0,0 +1,8 @@
+/* eslint-disable react-native/no-color-literals */
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  contentStyle: {
+    backgroundColor: '#F2F2F2'
+  }
+})

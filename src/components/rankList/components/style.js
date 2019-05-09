55cfdf7b92abd6ef1856e92b8a775e215eb6848c
MedalSUN <2496667187@qq.com>
2019-05-09 12:15:55 +0800

    榜单模块的组件样式

--- src/components/rankList/components/style.js
+++ src/components/rankList/components/style.js
@@ -25,5 +25,54 @@ export default StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center'
+  },
+
+  // ========= ShooterList的样式
+  titleLevel: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  titleBox: {
+    width: '30%',
+    height: 50
+  },
+  titleTextStyle: {
+    fontSize: 18,
+    color: '#808080',
+    fontFamily: 'Helvetica Neue'
+  },
+  // ========== ShooterListItem的样式
+  dataLevel: {
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  dataLevelFlexStart: {
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+  imgSize: {
+    width: 30,
+    height: 30,
+    marginRight: 10,
+    borderWidth: 1,
+    borderRadius: 15
+  },
+  listItemBox: {
+    width: '30%'
+  },
+  shooterText: {
+    fontSize: 15,
+    color: '#969696',
+    fontFamily: 'Helvetica Neue'
+  },
+  bottomLevel: {
+    width: 30,
+    height: 1,
+    backgroundColor: '#808080'
   }
 })

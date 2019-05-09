2e80068308dbc7d4bb986456e0496a79b9a9ab3e
MedalSUN <2496667187@qq.com>
2019-05-09 12:16:11 +0800

    增加榜单模块的query数据

--- src/gql/queries.js
+++ src/gql/queries.js
@@ -12,4 +12,56 @@ query allImages{
 }
 `
 
-// 获取所有赛事
+// 获取射手榜的数据
+export const ALL_SHOOTER = gql`
+query allShooterLists($orderBy: [ShooterListsOrderBy!] ){
+  allShooterLists(orderBy: $orderBy){
+    nodes{
+      shooterId
+      goalNum
+      personByShooterId{
+        playerName,
+        imageByPlayerImg{
+          url
+        }
+      }
+    }
+  }
+}`
+
+// 获取助攻榜的数据
+export const ALL_ASSIST = gql`
+query allAssistLists($orderBy: [AssistListsOrderBy!] ){
+  allAssistLists(orderBy: $orderBy){
+    nodes{
+      assistId,
+      assistNum,
+      personByAssistId{
+        playerName,
+        imageByPlayerImg{
+          url
+        }
+      }
+    }
+  }
+}
+`
+
+// 获取积分榜的数据
+export const ALL_SCORE = gql`
+query allScores($orderBy: [ScoresOrderBy!]){
+  allScores(orderBy: $orderBy){
+    nodes{
+      teamScore,
+      teamId,
+      footballTeamByTeamId{
+        id,
+        teamName,
+        imageByTeamLogo{
+          url
+        }
+      }
+    }
+  }
+}
+`

460815ac52e1c6bae851296b3b169b7c206b0193
MedalSUN <2496667187@qq.com>
2019-05-09 17:23:09 +0800

    赛事模块主页（Query数据）

--- src/components/race/RaceMainPage.js
+++ src/components/race/RaceMainPage.js
@@ -2,15 +2,36 @@ import React from 'react'
 import { View, TouchableOpacity, Text } from 'react-native'
 import { Container, Content } from 'native-base'
 import RaceItem from './components/RaceItem'
+import { Query } from 'react-apollo'
+import Styles from './style'
+import { ALL_MATCH_SCHEDULE } from '../../gql/queries'
 
 export default class RaceMainPage extends React.Component {
   render () {
-    return (
-      <Container>
-        <Content>
-          <RaceItem/>
-        </Content>
-      </Container>
-    )
+    return <Query query={ALL_MATCH_SCHEDULE} variables={{ orderBy: 'ORDER_NUMBER_ASC' }}>
+      {({ data, error, loading }) => {
+        if (loading) return <Text>Loading...</Text>
+        if (error) return <Text>`Error! ${error.message}`</Text>
+        return (
+          <Container>
+            <Content style={Styles.contentStyle}>
+              <View>
+                {data.allMatchSchedules.nodes.map((p, i) => {
+                  return (
+                    <RaceItem key={i} teamAName={p.footballTeamByTeamA.teamName} teamBName={p.footballTeamByTeamB.teamName}
+                      teamALogo={p.footballTeamByTeamA.imageByTeamLogo.url} matchId={p.id}
+                      teamBLogo={p.footballTeamByTeamB.imageByTeamLogo.url}
+                      matchDate={p.matchDate} matchLocation={p.footballCourtByMatchLocation.courtName} wheelNumber={p.wheelNumber}/>
+                  )
+                })
+                }
+
+              </View>
+            </Content>
+          </Container>
+
+        )
+      }}
+    </Query>
   }
 }

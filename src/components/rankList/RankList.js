c5128dc4a6d7513bba25db7bda46e069cc2929e7
MedalSUN <2496667187@qq.com>
2019-05-09 12:13:17 +0800

    榜单模块主页拆分组件

--- src/components/rankList/RankList.js
+++ src/components/rankList/RankList.js
@@ -3,21 +3,46 @@ import { Image, Text, TouchableOpacity } from 'react-native'
 import { Container, Content, View } from 'native-base'
 import RankNavigator from './components/RankNavigator'
 
+// 引入三个榜单组件
+import ShooterList from './components/ShooterList'
+import AssistList from './components/AssistList'
+import ScoreList from './components/ScoreList'
+
 class RankList extends Component {
   static navigationOptions = (
     (props) => {
       return {
-        headerTitle: <RankNavigator/>
+        headerTitle: <RankNavigator navigation={props.navigation}/>
       }
     }
   )
+  constructor (props) {
+    super(props)
+    this.state = {
+      showItem: 'A'
+    }
+  }
+
+  // 函数：获取标题组件的点击的标题
+  getNavItem = (value) => {
+    console.log(value)
+    this.setState({
+      showItem: value
+    })
+  }
+
+  componentDidMount = () => {
+    this.props.navigation.setParams({ getNavItem: this.getNavItem })
+  }
 
   render () {
     return (
       <Container>
         <Content>
           <View>
-            <Text>榜单</Text>
+            {this.state.showItem === 'A' && <ShooterList/> }
+            {this.state.showItem === 'B' && <AssistList/> }
+            {this.state.showItem === 'C' && <ScoreList/> }
           </View>
         </Content>
       </Container>

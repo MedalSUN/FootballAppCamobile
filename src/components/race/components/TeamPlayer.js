8b1d94eb8f268041b23d7dca147361ba6eaab46e
MedalSUN <2496667187@qq.com>
2019-05-13 22:27:24 +0800

    赛事详情的主客队球员组件

---
+++ src/components/race/components/TeamPlayer.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import { View, Text, Image } from 'react-native'
+import { Container, Content } from 'native-base'
+import { Query } from 'react-apollo'
+import Styles from './style'
+import { ALL_TEAM_PLAYER } from '../../../gql/queries'
+
+export default class TeamPlayer extends React.Component {
+  render () {
+    const { teamId, teamName, teamLogo } = this.props
+    return <Query query={ALL_TEAM_PLAYER} variables={{ condition: { teamId: teamId } }}>
+      {({ data, error, loading }) => {
+        if (loading) return <Text>Loading...</Text>
+        if (error) return <Text>`Error! ${error.message}`</Text>
+        return (
+          <View style={Styles.contentStyle}>
+            {/* 球队logo以及球队名称 */}
+            <View style={Styles.teamLogoBox}>
+              <Image style={Styles.teamLogoSize} source={{ uri: teamLogo }}/>
+              <Text style={Styles.teamLogoNameText}>{teamName}</Text>
+            </View>
+            {/* 球队成员 */}
+            <View>
+              <View style={Styles.timeEventBox}>
+                <Text style={Styles.otherText}>球队成员</Text>
+              </View>
+              {/* 展示球队的所有球员 */}
+              <View style={Styles.allPlayerContainer}>
+                {
+                  data.allPersonTeams.nodes.map((p, i) => {
+                    return (
+                      <View key={i} style={[Styles.timeEventBox, Styles.marginTop10]}>
+                        <Image style={Styles.playerHeaderImg} source={{ uri: p.personByPersonId.imageByPlayerImg.url }}/>
+                        <Text style={Styles.playerNameText}>{p.personByPersonId.playerName}</Text>
+                      </View>
+                    )
+                  })
+                }
+              </View>
+            </View>
+          </View>
+        )
+      }}
+    </Query>
+  }
+}

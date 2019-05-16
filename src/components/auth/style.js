62f402c9923218b1d186a517bc5e2f6811238447
MedalSUN <2496667187@qq.com>
2019-05-16 19:06:29 +0800

    增加注册页面的样式

--- src/components/auth/style.js
+++ src/components/auth/style.js
@@ -52,5 +52,49 @@ export default StyleSheet.create({
   whiteText: {
     color: 'rgb(255,255,255)',
     fontSize: 15
+  },
+  // ========注册页面样式
+  contentStyle: {
+    backgroundColor: '#F2F2F2'
+  },
+  registerContent: {
+    marginTop: 20,
+    width: '90%'
+    // backgroundColor: 'yellow'
+  },
+  columnFlex: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  registerImg: {
+    width: 200,
+    height: 200,
+    marginTop: 20,
+    marginLeft: 20,
+    borderWidth: 1,
+    borderRadius: 100
+  },
+  registerButton: {
+    marginTop: 50,
+    marginLeft: 30,
+    width: 150,
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#FF9900',
+    borderRadius: 20,
+    backgroundColor: '#FF9900',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  completeText: {
+    marginLeft: 20,
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'rgb(51,51,51)',
+    fontSize: 15
   }
 })
